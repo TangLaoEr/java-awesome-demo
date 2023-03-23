2d60d743a0d31727ec1f3c5b4b9d65319513844d
tanglaoer <1585701160@qq.com>
2023-03-23 23:49:00 +0800

    目录整理、划分子目录

---
+++ spring-cloud-demo/gateway/src/main/java/cn/tanglaoer/demo/config/MyConfig.java
@@ -0,0 +1,20 @@
+package cn.tanglaoer.demo.config;
+
+
+/**
+ * @author： tks
+ * @date： 2023/3/8
+ */
+// @Configuration
+// @Slf4j
+public class MyConfig {
+    // @Primary
+    // @Bean("gatewaySwaggerRoute2")
+    // public RouterFunction<ServerResponse> gatewaySwaggerRoute(Knife4jGatewayProperties knife4jGatewayProperties) {
+    //     log.info("init gateway swagger resources.");
+    //     RouterFunction<ServerResponse> build = RouterFunctions.route().GET("/swagger-resources", (request) -> {
+    //         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(knife4jGatewayProperties.build());
+    //     }).build();
+    //     return build;
+    // }
+}
---
+++ spring-cloud-demo/rocketmq-demo/src/main/java/cn/tanglaoer/demo/config/MQAdminConfig.java
@@ -0,0 +1,12 @@
+package cn.tanglaoer.demo.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author： tks
+ * @date： 2023/3/20
+ */
+@Component
+public class MQAdminConfig {
+
+}
